e06226444eac2bcdb23238c7a41ec268e4c93b0c
maurocelcan <maurocelcan@gmail.com>
2023-10-05 12:35:06 -0300

    0.0.7 commit

--- src/scripts/faq.js
+++ src/scripts/faq.js
@@ -1,10 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const faqContainers = document.querySelectorAll('.faq-container');
-    faqContainers.forEach(function (container) {
-        const answer = container.querySelector('.faq-answer');
-        
-        container.addEventListener('click', function () {
-            answer.classList.toggle('expanded');
+function setupFaqToggle() {
+        const faqContainers = document.querySelectorAll('.faq-container');
+        faqContainers.forEach(function (container) {
+            const answer = container.querySelector('.faq-answer');
+            
+            container.addEventListener('click', function () {
+                answer.classList.toggle('expanded');
+            });
         });
-    });
+};
+
+document.addEventListener("astro:page-load", () => {
+    setupFaqToggle();
 });
+
