603e72d97173747bfca2087e91aac8de13ada3d3
maurocelcan <maurocelcan@gmail.com>
2023-10-05 15:14:29 -0300

    0.0.8 commit

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,14 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('expanded');
+function setupMenuToggle() {
+    document.querySelector('.hamburger').addEventListener('click', () => {
+        document.querySelector('.nav-links').classList.toggle('expanded');
+    });    
+};
+
+document.addEventListener("astro:page-load", () => {
+    setupMenuToggle();
 });
+
+
+
+
+
